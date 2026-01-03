50c7bc8a7fe6d3e119a53fb661b8ff17ed01d03d
kaikeventura <kaike.gnr@hotmail.com>
2026-01-03 20:50:53 -0300

    Configurado cors para s3 aws no localstack

--- scripts/init-aws.sh
+++ scripts/init-aws.sh
@@ -4,4 +4,9 @@ awslocal s3api create-bucket \
     --bucket petgram-bucket \
     --region us-east-1
 
-echo "----------- S3 bucket created -----------"
+echo "----------- Applying CORS policy to the bucket -----------"
+awslocal s3api put-bucket-cors \
+    --bucket petgram-bucket \
+    --cors-configuration file:///etc/localstack/init/ready.d/cors.json
+
+echo "----------- S3 bucket created and configured -----------"
