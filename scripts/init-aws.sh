57155178a3b4b6a5ea2c50f1bb127a138476f86c
kaikeventura <kaike.gnr@hotmail.com>
2026-01-03 13:09:21 -0300

    Removido script

--- scripts/init-aws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "----------- Creating S3 bucket -----------"
-awslocal s3api create-bucket \
-    --bucket petgram-bucket \
-    --region us-east-1
-
-echo "----------- S3 bucket created -----------"
